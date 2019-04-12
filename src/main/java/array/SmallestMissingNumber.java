8797e881d86c0bc8b879e8737ba0044af0c9691e
renuka rajput <renukarajput707.com>
2019-04-12 19:22:05 +0530

    Smallest integer in sorted array using linear search and tests

--- src/main/java/array/SmallestMissingNumber.java
+++ src/main/java/array/SmallestMissingNumber.java
@@ -1,7 +1,7 @@
 package array;
 
 public class SmallestMissingNumber {
-// 1 2 3 5
+    // 1 2 3 5
     public int find(int[] input, int maxRange) {
         int start = 0;
         int end = input.length - 1;
@@ -13,12 +13,29 @@ public class SmallestMissingNumber {
                 start = mid + 1;
             }
 
-            if (input[i] != i){
+            if (input[i] != i) {
                 return i;
             }
         }
-        if (input[input.length-1] < maxRange){
-            return input[input.length-1]+1;
+        if (input[input.length - 1] < maxRange) {
+            return input[input.length - 1] + 1;
+        }
+        return 0;
+    }
+
+    public int findMissingNumber(int[] input, int maxRange) {
+
+        if (input[0] != 0)
+            return 0;
+
+        for (int i = 0; i < input.length-1; i++) {
+            int difference = input[i+1]-input[i];
+            if (difference > 1){
+                return input[i]+1;
+            }
+        }
+        if (input[input.length - 1] < maxRange) {
+            return input[input.length - 1] + 1;
         }
         return 0;
     }
--- src/test/java/array/SmallestMissingNumberTest.java
+++ src/test/java/array/SmallestMissingNumberTest.java
@@ -13,29 +13,34 @@ public class SmallestMissingNumberTest {
     public void find_1() {
         int[] arr = {0, 1, 2, 6, 9};
         assertEquals(3, smallestMissingNumber.find(arr, 10));
+        assertEquals(3, smallestMissingNumber.findMissingNumber(arr, 10));
     }
 
     @Test
     public void find_2() {
         int[] arr = {4, 5, 10, 11};
         assertEquals(0, smallestMissingNumber.find(arr, 12));
+        assertEquals(0, smallestMissingNumber.findMissingNumber(arr, 12));
     }
 
     @Test
     public void find_3() {
         int[] arr = {0, 1, 2, 3};
         assertEquals(4, smallestMissingNumber.find(arr, 5));
+        assertEquals(4, smallestMissingNumber.findMissingNumber(arr, 5));
     }
 
     @Test
     public void find_4() {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 10};
         assertEquals(8, smallestMissingNumber.find(arr, 11));
+        assertEquals(8, smallestMissingNumber.findMissingNumber(arr, 11));
     }
 
     @Test
     public void find_5() {
         int[] arr = {0, 1, 2, 3, 9, 13, 15, 18, 19};
         assertEquals(4, smallestMissingNumber.find(arr, 20));
+        assertEquals(4, smallestMissingNumber.findMissingNumber(arr, 20));
     }
 }
